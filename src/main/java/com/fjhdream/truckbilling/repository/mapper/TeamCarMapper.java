2c9a820c86ed8c7d6f3f16957740d80f5e3db609
fjhdream <fjh_dream@163.com>
2023-01-20 22:07:36 +0800

    添加默认status

--- src/main/java/com/fjhdream/truckbilling/repository/mapper/TeamCarMapper.java
+++ src/main/java/com/fjhdream/truckbilling/repository/mapper/TeamCarMapper.java
@@ -4,6 +4,7 @@ import com.fjhdream.truckbilling.controller.entity.TeamCarRequest;
 import com.fjhdream.truckbilling.controller.entity.TeamCarResponse;
 import com.fjhdream.truckbilling.repository.entity.Team;
 import com.fjhdream.truckbilling.repository.entity.TeamCar;
+import com.fjhdream.truckbilling.repository.enums.UseStatusEnum;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -14,12 +15,13 @@ import java.util.Set;
 /**
  * @author carota
  */
-@Mapper
+@Mapper(imports = {UseStatusEnum.class})
 public interface TeamCarMapper {
     TeamCarMapper INSTANCE = Mappers.getMapper(TeamCarMapper.class);
 
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "status", expression = "java(UseStatusEnum.INUSE)")
     TeamCar teamCarFromTeamCarRequestAndTeam(TeamCarRequest teamCarRequest, Team team);
 
     @Mapping(target = "carId", source = "id")
